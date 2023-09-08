b24d12000a490cd6b9f6a3ec84860cd3040890ab
pouya karimian <karimian.pouya96@gmial.com>
2023-09-08 09:53:22 +0330

    Add ICustomerWriteRepository

---
+++ src/Mc2.CrudTest.Domain/Aggregates/Customer/ICustomerWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mc2.CrudTest.Domain.BaseDomain;
+using Mc2.CrudTest.Domain.ValueObjects;
+
+namespace Mc2.CrudTest.Domain.Aggregates.Customer
+{
+    public interface ICustomerWriteRepository: IWriteOnlyRepository<Customer,Guid>
+    {
+        Task<bool> ExistsByEmailAsync(Email email);
+
+        Task<bool> ExistsByEmailAsync(Email email, Guid currentId);
+    }
+}
