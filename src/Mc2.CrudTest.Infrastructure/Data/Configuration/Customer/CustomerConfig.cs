025219378c10e9e1b131c57f79348b5b7beaa746
pouya karimian <karimian.pouya96@gmial.com>
2023-09-08 10:57:00 +0330

    Add data, context, customer configuration

---
+++ src/Mc2.CrudTest.Infrastructure/Data/Configuration/Customer/CustomerConfig.cs
@@ -0,0 +1,22 @@
+﻿using Mc2.CrudTest.Infrastructure.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Infrastructure.Data.Configuration.Customer
+{
+    public class CustomerConfig : IEntityTypeConfiguration<Domain.Aggregates.Customer.Customer>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Aggregates.Customer.Customer> builder)
+        {
+            builder.ConfigureBaseEntity();
+            builder.Property(c => c.FirstName).HasColumnType("VARCHAR(100)");
+            builder.Property(c => c.LastName).HasColumnType("VARCHAR(100)");
+            builder.Property(c => c.PhoneNumber).HasColumnType("VARCHAR(25)");
+            builder.Property(c => c.BankAccountNumber).IsRequired().HasColumnType("VARCHAR(35)");
+            builder.Property(c => c.Email).IsRequired().HasColumnType("NVARCHAR(100)");
+            builder.Property(c => c.DateOfBirth).HasColumnType("datetime2");
+            builder.HasIndex(c => new { c.FirstName, c.LastName, c.DateOfBirth }).IsUnique();
+            builder.HasIndex(c => c.Email).IsUnique();
+        }
+    }
+}
---
+++ src/Mc2.CrudTest.Infrastructure/Data/Context/CrudTestDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Mc2.CrudTest.Domain.Aggregates.Customer;
+using Mc2.CrudTest.Infrastructure.Data.Configuration.Customer;
+using Mc2.CrudTest.Infrastructure.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Infrastructure.Data.Context
+{
+    public class CrudTestDbContext : DbContext
+    {
+        private const string Collation = "Latin1_General_CI_AI";
+
+        protected CrudTestDbContext(DbContextOptions<CrudTestDbContext> dbOptions) : base(dbOptions)
+        {
+        }
+        public DbSet<Customer> Customers => Set<Customer>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.UseCollation(Collation).RemoveCascadeDeleteConvention();
+            modelBuilder.ApplyConfiguration(new CustomerConfig());
+        }
+    }
+}
